99003823898699aadb1156557bc9ce62bae3ecb2
João Pedro <joao_pedro01@terra.com.br>
2022-04-14 17:38:07 -0300

    Update tarefa2

---
+++ tarefa2/frmPrincipal.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Tarefa2
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemConsClie = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemConsProd = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(800, 24);
+            this.menuPrincipal.TabIndex = 0;
+            this.menuPrincipal.Text = "menuPrincipal";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemConsClie,
+            this.itemConsProd,
+            this.itemSair});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // itemConsClie
+            // 
+            this.itemConsClie.Name = "itemConsClie";
+            this.itemConsClie.Size = new System.Drawing.Size(180, 22);
+            this.itemConsClie.Text = "Cliente";
+            this.itemConsClie.Click += new System.EventHandler(this.itemConsClie_Click);
+            // 
+            // itemConsProd
+            // 
+            this.itemConsProd.Name = "itemConsProd";
+            this.itemConsProd.Size = new System.Drawing.Size(180, 22);
+            this.itemConsProd.Text = "Produto";
+            this.itemConsProd.Click += new System.EventHandler(this.itemConsProd_Click);
+            // 
+            // itemSair
+            // 
+            this.itemSair.Name = "itemSair";
+            this.itemSair.Size = new System.Drawing.Size(180, 22);
+            this.itemSair.Text = "Sair";
+            this.itemSair.Click += new System.EventHandler(this.itemSair_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuPrincipal;
+            this.Name = "frmPrincipal";
+            this.Text = "Formulario Principal";
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemConsClie;
+        private System.Windows.Forms.ToolStripMenuItem itemConsProd;
+        private System.Windows.Forms.ToolStripMenuItem itemSair;
+    }
+}
+
---
+++ tarefa2/frmPrincipal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarefa2
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            this.WindowState = FormWindowState.Maximized;// Inicia o código em tela cheia
+            InitializeComponent();
+        }
+
+        private void itemConsClie_Click(object sender, EventArgs e)
+        {
+            frmPrincipal form = new frmPrincipal();
+            frmCliente frm = new frmCliente();
+            frm.Show(); //Chama o forms
+            
+        }
+
+        private void itemConsProd_Click(object sender, EventArgs e)
+        {
+            frmProduto frm = new frmProduto();
+            frm.Show(); //Chama o forms
+        }
+
+        private void itemSair_Click(object sender, EventArgs e)
+        {
+            var sair = MessageBox.Show("Deseja sair?", "APP Teste", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (sair == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Você escolhe ficar!", "Eletiva Prog. BD", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+    }
+}
